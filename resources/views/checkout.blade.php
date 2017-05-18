ed656adf21ed51b0d0e1ae39a11c32ca61b82475
camduong <camduongsgu@gmail.com>
2017-05-18 17:24:39 +0700

    quên sửa j r

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,10 +1,4 @@
-@extends('layouts.appsss')
 
-@section('title')
-  Laravel Shopping Cart
-@endsection
-
-@section('content')
   <div class="row">
     <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
       <h1>Checkout</h1>
@@ -32,4 +26,3 @@
       </form>
     </div>
   </div>
-@endsection
