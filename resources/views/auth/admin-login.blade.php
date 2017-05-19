9980144c0b3c43613a318eca84ccbaca1b010e53
MasterZero94 <masterzero1208@gmail.com>
2017-05-19 15:47:51 +0700

    làm customer+fix shopping cart

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.adapp')
 
 @section('content')
 <div class="container">
---
+++ resources/views/customer/edit.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.admain')
+
+@section('title', ' View Customer')
+
+@section('content')
+    <div class="right_col" role="main">
+		<div class="row">
+			{!! Form::model($customer, ['route' => ['customer.update', $customer->id], 'method' => 'PUT']) !!}
+			<h1>Chi tiết đơn hàng</h1>
+      <div class="col-md-8">
+          <div class="panel panel-default">
+              <div class="panel-body">
+                  <ul class="list-group">
+                  @foreach($customer->cart->items as $item)
+                  <li class="list-group-item"><span class="badge">Giá: {{ number_format($item['price']) }} VNĐ</span>
+                  {{ $item['item']['name'] }} | {{ $item['qty'] }} Vé
+                  </li>
+                  @endforeach
+                  </ul>
+              </div>
+              <div class="panel-footer text-right">
+                  <strong>Tổng giá: {{ number_format($customer->cart->totalPrice) }} VNĐ</strong>
+              </div>
+          </div>
+      </div>
+
+			<div class="col-md-4">
+				<div class="well">
+          <div class="row">
+						<div class="col-sm-12">
+							<span class="lead">Tình trạng: </span>
+              <select name="status">
+                <option value=" Chưa Xử Lý">Chưa xử lý</option>
+                <option value="Xử Lý">Xử lý</option>
+                <option value="Hủy">Hủy</option>
+              </select>
+						</div>
+					</div>
+          
+					<dl class="dl-horizontal">
+						<dt>Ngày tạo:</dt>
+						<dd>{{ date('l d/m/Y h:i a', strtotime($customer->created_at)) }}</dd>
+					</dl>
+
+					<dl class="dl-horizontal">
+						<dt>Ngày cập nhật:</dt>
+						<dd>{{ date('l d/m/Y h:i a',strtotime($customer->updated_at)) }}</dd>
+					</dl>
+					<hr>
+					<div class="row">
+						<div class="col-sm-6">
+							{{ Form::submit('Lưu', ['class' => 'btn btn-success btn-block']) }}
+						</div>
+						<div class="col-sm-6">
+							{!! Html::linkRoute('customer.index', 'Thoát', null, ['class' => 'btn btn-danger btn-block']) !!}
+						</div>
+					</div>
+
+				</div>
+			</div>
+			{!! Form::close() !!}
+		</div> <!--end of .row(form)-->
+	</div>
+@endsection
