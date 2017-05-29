1cdd25bb7ace1db627442e62dca51b1bf4957cd6
camduong <camduongsgu@gmail.com>
2017-05-29 15:52:00 +0700

    asd
    sadasdasd

---
+++ app/Tour.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    public function hotels()
+    {
+        return $this->belongsTo('App\Hotel', 'hotel_id');
+    }
+
+    public function depart_locations()
+    {
+        return $this->belongsTo('App\Location', 'depart_location_id');
+    }
+
+    public function dest_locations()
+    {
+        return $this->belongsTo('App\Location', 'dest_location_id');
+    }
+
+    public function vehicles()
+    {
+        return $this->belongsTo('App\Vehicle', 'vehicle_id');
+    }
+}
